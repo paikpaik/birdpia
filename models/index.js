101b8c47949cbdd5f47abdc89d99bf136cea71c2
paikpaik <woduf9311@gmail.com>
2023-09-05 17:15:45 +0900

    Fix: sequelize index.js mysql db 연결 자동화로 코드 수정

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
-const User = require("./user");
-const Post = require("./post");
-const Hashtag = require("./hashtag");
+const fs = require("fs");
+const path = require("path");
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 const db = {};
@@ -13,15 +12,23 @@ const sequelize = new Sequelize(
 );
 
 db.sequelize = sequelize;
-db.User = User;
-db.Post = Post;
-db.Hashtag = Hashtag;
 
-User.initiate(sequelize);
-Post.initiate(sequelize);
-Hashtag.initiate(sequelize);
-User.associate(db);
-Post.associate(db);
-Hashtag.associate(db);
+const basename = path.basename(__filename); // index.js
+fs.readdirSync(__dirname) // models
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    db[model.name] = model;
+    model.initiate(sequelize);
+  });
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 module.exports = db;
