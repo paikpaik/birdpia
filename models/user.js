1529c0e91cc190a819a9d2a3586a183aa316708a
paikpaik <woduf9311@gmail.com>
2023-09-05 16:39:45 +0900

    Init: sequelize mysql db index, user, post, hashtag 초기 구성

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init(
+      {
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+          unique: true,
+        },
+        nick: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        provicer: {
+          type: Sequelize.ENUM("local", "kakao"),
+          allowNull: false,
+          defaultValue: "local",
+        },
+        snsId: {
+          type: Sequelize.STRING(30),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true, // createdAt, updatedAt
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: true, // deletedAt
+        charset: "utf8mb4",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {}
+}
+
+module.exports = User;
