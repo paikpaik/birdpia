a2f954616a92af5062016465781002f2736554c1
paikpaik <woduf9311@gmail.com>
2023-09-05 17:36:09 +0900

    Add: db User, Post, Hashtag associate 추가

--- models/post.js
+++ models/post.js
@@ -26,7 +26,10 @@ class Post extends Sequelize.Model {
     );
   }
 
-  static associate(db) {}
+  static associate(db) {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
+  }
 }
 
 module.exports = Post;
