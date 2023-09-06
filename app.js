1d2a4e4d6b61eab3478c2ecf1dcc1a2237ac4d0a
paikpaik <woduf9311@gmail.com>
2023-09-06 13:28:08 +0900

    Fix: 주석 제거

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(
     },
   })
 );
-app.use(passport.initialize()); // passport는 반드시 session 바로 아래에
+app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", pageRouter);
