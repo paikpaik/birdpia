cd5679683bbcb8a57e8cb960adf530e5ece881de
paikpaik <woduf9311@gmail.com>
2023-09-07 13:53:07 +0900

    Add: 태그별 검색 추가

--- controllers/page.js
+++ controllers/page.js
@@ -1,5 +1,7 @@
 const Post = require("../models/post");
 const User = require("../models/user");
+const Hashtag = require("../models/hashtag");
+const e = require("express");
 
 exports.renderProfile = (req, res, next) => {
   res.render("profile", { title: "내 정보 - BirdPia" });
@@ -20,6 +22,30 @@ exports.renderMain = async (req, res, next) => {
     });
     res.render("main", {
       title: "BirdPia",
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.renderHashtag = async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({
+        include: [{ model: User, attributes: ["id", "nick"] }],
+        order: [["createdAt", "DESC"]],
+      });
+    }
+    res.render("main", {
+      title: `${query} | BirdPia`,
       twits: posts,
     });
   } catch (error) {
