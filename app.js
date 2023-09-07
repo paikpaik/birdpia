36fc5385ad0b0f3253a9a944031af515cebd9e8d
paikpaik <woduf9311@gmail.com>
2023-09-07 13:32:02 +0900

    Add: follow 추가

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ dotenv.config();
 const pageRouter = require("./routes/page");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
 const passportConfig = require("./passport");
 
 const app = express();
@@ -55,6 +56,7 @@ app.use(passport.session());
 app.use("/", pageRouter);
 app.use("/auth", authRouter);
 app.use("/post", postRouter);
+app.use("/user", userRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
